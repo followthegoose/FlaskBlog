b1f4992b5083cb398f7f7021254bfeb8c4963464
followthegoose <googoosik88@protonmail.com>
2020-05-14 16:43:45 +0400

    yo

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired, ValidationError, EqualTo
+from wtforms.validators import DataRequired, ValidationError, EqualTo, Email
 from app.models import User
 
 
@@ -17,7 +17,7 @@ class RegistrationForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     password2 = PasswordField(
         'Repeat Password', validators=[DataRequired(), EqualTo('password')])
-    sumbit = SubmitField('Register')
+    submit = SubmitField('Register')
 
     def validate_username(self, username):
         user = User.query.filter_by(username=username.data).first()
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from app import app,db
-from app.forms import LoginForm
+from app.forms import LoginForm, RegistrationForm
 from flask import render_template, flash, redirect, url_for, request
 from flask_login import current_user, login_user, logout_user, login_required
 from app.models import User
@@ -57,6 +57,22 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
+
+"""РЕГИСТРИАЦИЯ"""
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Поздравляшки! Теперь ты зарегистрирован!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
 
 
 
